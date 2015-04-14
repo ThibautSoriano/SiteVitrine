efd6f577b3ec9d409f213c3896710111ccf7b31d
webdev <webdev@webDevVM>
2015-04-14 23:09:23 +0200

    site vitrine

---
+++ showcase/app/scripts/controllers/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name showcaseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the showcaseApp
+ */
+angular.module('showcaseApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $http.get('data/infos.json').success(function(data) {
+      $scope.modele = data;
+    });
+  });
